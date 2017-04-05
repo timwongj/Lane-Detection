feb1c817ed77e4eadfb05571fc50a1d596884864
cbabroski <cjbabroski@gmail.com>
2017-04-05 15:19:38 -0400

    added horizontal lines for precision

--- src/lanecalibration.py
+++ src/lanecalibration.py
@@ -4,15 +4,15 @@ import matplotlib.image as mpimg
 import numpy as np
 
 class LaneCalibration(object):
-    def __init__(self, image):
+    def __init__(self, img):
         self.mclicks = 0 # Tracks number of mouse clicks
         self.click_coords = [] # Tracks mouse click coordinates
-        self.img = cv2.imread(image)
+        self.img = img
 
     def run(self):
         # Set mouse click event
         cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-        cv2.setMouseCallback('image', self.draw_circle)
+        cv2.setMouseCallback('image', self.display_points)
 
         # Wait for four mouse clicks 
         while(self.mclicks < 4):
@@ -32,12 +32,12 @@ class LaneCalibration(object):
         self.click_coords.sort(key = lambda row: (row[1],row[0]))
         return np.asarray(self.click_coords)
 
-    def draw_circle(self,event,x,y,flags,param):
+    def display_points(self,event,x,y,flags,param):
         # Text/circle parameters
         text = "(%d,%d)" %(x,y)
         radius = 7
         fill = -1
-        pos = (x+6, y+6)
+        pos = (x-20, y-15)
         font = cv2.FONT_HERSHEY_SIMPLEX
         size = 0.6
         color = (50,50,255)
@@ -49,8 +49,8 @@ class LaneCalibration(object):
             if self.mclicks <= 4:
                 cv2.circle(self.img, (x,y), radius, color, fill)
                 cv2.putText(self.img, text, pos, font, size, color, thickness)
-                mouseX,mouseY = x,y
-                self.click_coords.append([mouseX,mouseY])
+                cv2.line(self.img, (0,y), (self.img.shape[1],y), color)
+                self.click_coords.append([x,y])
 
     def display_image(self):
         # Text parameters
@@ -58,7 +58,7 @@ class LaneCalibration(object):
         pos = (200,200)
         font = cv2.FONT_HERSHEY_SIMPLEX
         size = 1
-        color = (0,0,0)
+        color = (255,255,255)
         thickness = 2
 
         # Add text, show image
@@ -67,7 +67,8 @@ class LaneCalibration(object):
 
 
 if __name__ == '__main__':
-    LaneCalibration = LaneCalibration('../output_images/undistorted.jpg')
+    image = cv2.imread('../output_images/thresholded.jpg')
+    LaneCalibration = LaneCalibration(image)
     points = LaneCalibration.run()
     print(points)
 
